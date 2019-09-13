4608e5b9fec9eb09b76515d4f0f501315a1c66c3
harrisonerd@icloud.com <harrisonerd@icloud.com>
2019-09-12 21:20:51 -0400

    Updated doc strings and ordered functions in a mor logical order. Small website improvements showing the abilty to use pydoc to view the docstrings

--- kenobi.py
+++ kenobi.py
@@ -2,40 +2,39 @@
 # -*- coding: utf-8 -*-
 
 """
-   KenobiDB is a stupid and small document based DB, supporting very simple
-   usage including insertion, removal and basic search. It useses pickle.
-   Written by Harrison Erd (https://patx.github.io/)
-   https://patx.github.io/kenobi/
-
-
-Copyright 2019 Harrison Erd
-
-Redistribution and use in source and binary forms, with or without 
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, 
-this list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice, 
-this list of conditions and the following disclaimer in the documentation 
-and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its 
-contributors may be used to endorse or promote products derived from this 
-software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
-IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
-PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
-EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
-PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
-OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
-OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    KenobiDB is a stupid and small document based DB, supporting very simple
+    usage including insertion, removal and basic search. It useses pickle.
+    Written by Harrison Erd (https://patx.github.io/)
+    https://patx.github.io/kenobi/
 """
+
+# Copyright 2019 Harrison Erd
+
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, 
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice, 
+# this list of conditions and the following disclaimer in the documentation 
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its 
+# contributors may be used to endorse or promote products derived from this 
+# software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import pickle
 import os
@@ -43,7 +42,7 @@ import os
 
 class KenobiDB(object):
 
-    def __init__(self, file, auto_save=True):
+    def __init__(self, file, auto_save=False):
         """Creates a database object and loads the data from the location path.
         If the file does not exist it will be created. Also allows you to set
         auto_save to True or False (default=False). If auto_save is set to
@@ -56,44 +55,53 @@ class KenobiDB(object):
         else:
             self.db = []
 
+    # Utility functions
+
     def save_db(self):
-        """Save the database to a file and returns True"""
+        """Save the database to a file, return True."""
         pickle.dump(self.db, open(self.file, 'wb'))
-
-    def all(self):
-        """Return a list of all documents in the database"""
-        return self.db
-
-    def purge(self):
-        """Remove all documents from the database"""
-        self.db = []
-        if self.auto_save:
-            self.save_db()
         return True
 
-    def insert(self, document):
-        """Add a document (a python dict) to the database and return True"""
-        self.db.append(document)
+    def _autosave(self):
+        """Force dump the database into the file."""
         if self.auto_save:
             self.save_db()
+
+    # Add/delete functions
+
+    def insert(self, document):
+        """Add a document (a python dict) to the database and return True."""
+        self.db.append(document)
+        self._autosave()
         return True
 
     def remove(self, key, value):
         """Remove a document with the matching key: value pair as key
-        and value args given
+        and value args given, return document(s) that were removed.
         """
         result = []
         for document in self.db:
             if (key, value) in document.items():
                 result.append(document)
                 self.db.remove(document)
-        if self.auto_save:
-            self.save_db()
+        self._autosave()
         return result
+
+    def purge(self):
+        """Remove all documents from the database, return True."""
+        self.db = []
+        self._autosave()
+        return True
+
+    # Search functions
+
+    def all(self):
+        """Return a list of all documents in the database."""
+        return self.db
 
     def search(self, key, value):
         """Return a list of documents with key: value pairs matching the
-        given key and value args given
+        given key and value args given.
         """
         result = []
         for document in self.db:
@@ -102,8 +110,8 @@ class KenobiDB(object):
         return result
 
     def find_all(self, key, value):
-        """Return a list of documents with keys including at least one value
-        from the list value
+        """For use with a value that is a list. Return a list of documents
+        with keys including at least one match from the list value.
         """
         result = []
         for document in self.db:
@@ -114,8 +122,8 @@ class KenobiDB(object):
         return result
 
     def find_any(self, key, value):
-        """Return a list of documents with keys including all values
-        from the list value
+        """For use with a value that is a list. Return a list of documents 
+        with keys including all matches from the list value.
         """
         result = []
         for document in self.db:
