d98b8f6f3c72e9b1fe588d6d152c3c4e3605412d
patx <harrisonerd@icloud.com>
2019-12-23 14:39:18 -0500

    expaned website adding api docs and a walkthrough, updated readme file

--- kenobi.py
+++ kenobi.py
@@ -3,7 +3,7 @@
 
 """
     KenobiDB is a small document based DB, supporting very simple
-    usage including insertion, removal and basic search. Made with YAML.
+    usage including insertion, removal and basic search.
     Written by Harrison Erd (https://patx.github.io/)
     https://patx.github.io/kenobi/
 """
